df1bd8fc3e692c3db7d47e151c202473676d98ce
garvit-density <garvit.intern@density.exchange>
2024-01-17 01:39:03 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { useState } from 'react';
 
 const App = () => {
 
-  const [data,setData] = useState(['Apple', 'Banana', 'Orange', 'Grapes', 'Mango']);
+  const [data,setData] = useState(['Apple', 'Banana', 'Orange', 'Grapes', 'Mango','Guava','Lichi']);
 
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Autocomplete </h1>
+      <h1 className="text-2xl font-bold mb-4">Autocomplete - type fruit names eg-> Apple , Mango , Orange</h1>
       <h3 className="text-xl font-bold mb-4"> add item | use backspace to delete | use up-down keyboard arrow to navigate list | enter to select </h3>
       <Autocomplete data={data} setData={setData} />
     </div>
